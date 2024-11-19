b18acd0ec3759862d8906ebf7b10b0b37d63eb97
Felype <felyped03@gmail.com>
2024-11-19 16:34:02 -0300

    Update Lista3.sql

--- Banco_dados/Banco_dados-main/aula5/Lista3.sql
+++ Banco_dados/Banco_dados-main/aula5/Lista3.sql
@@ -44,22 +44,22 @@ FOREIGN KEY(cod_prod) REFERENCES produto(codigo)
 GO
 INSERT INTO fornecedor VALUES (1001,'Estrela','Brinquedo','41525898')
 INSERT INTO fornecedor VALUES (1002,'Lacta','Chocolate','42698596')
-INSERT INTO fornecedor VALUES (1003,'Asus','Inform·tica','52014596')
-INSERT INTO fornecedor VALUES (1004,'Tramontina','UtensÌlios DomÈsticos','50563985')
+INSERT INTO fornecedor VALUES (1003,'Asus','Inform√°tica','52014596')
+INSERT INTO fornecedor VALUES (1004,'Tramontina','Utens√≠lios Dom√©sticos','50563985')
 INSERT INTO fornecedor VALUES (1005,'Grow','Brinquedos','47896325')
 INSERT INTO fornecedor VALUES (1006,'Mattel','Bonecos','59865898')
-INSERT INTO cliente VALUES (33601,'Maria Clara','R. 1∞ de Abril',870,'96325874','15/08/2000')
+INSERT INTO cliente VALUES (33601,'Maria Clara','R. 1¬∞ de Abril',870,'96325874','15/08/2000')
 INSERT INTO cliente VALUES (33602,'Alberto Souza','R. XV de Novembro',987,'95873625','02/02/1985')
-INSERT INTO cliente VALUES (33603,'Sonia Silva','R. Volunt·rios da P·tria',1151,'75418596','23/08/1957')
-INSERT INTO cliente VALUES (33604,'JosÈ Sobrinho','Av. Paulista',250,'85236547','09/12/1986')
+INSERT INTO cliente VALUES (33603,'Sonia Silva','R. Volunt√°rios da P√°tria',1151,'75418596','23/08/1957')
+INSERT INTO cliente VALUES (33604,'Jos√© Sobrinho','Av. Paulista',250,'85236547','09/12/1986')
 INSERT INTO cliente VALUES (33605,'Carlos Camargo','Av. Tiquatira',9652,'75896325','25/03/1971')
-INSERT INTO produto VALUES (1,'Banco Imobili·rio',65.00,15,'Vers„o Super Luxo',1001)
-INSERT INTO produto VALUES (2,'Puzzle 5000 peÁas',50.00,5,'Mapas Mundo',1005)
-INSERT INTO produto VALUES (3,'Faqueiro',350.00,0,'120 peÁas',1004)
-INSERT INTO produto VALUES (4,'Jogo para churrasco',75.00,3,'7 peÁas',1004)
+INSERT INTO produto VALUES (1,'Banco Imobili√°rio',65.00,15,'Vers√£o Super Luxo',1001)
+INSERT INTO produto VALUES (2,'Puzzle 5000 pe√ßas',50.00,5,'Mapas Mundo',1005)
+INSERT INTO produto VALUES (3,'Faqueiro',350.00,0,'120 pe√ßas',1004)
+INSERT INTO produto VALUES (4,'Jogo para churrasco',75.00,3,'7 pe√ßas',1004)
 INSERT INTO produto VALUES (5,'Tablet',750.00,29,'Tablet',1003)
-INSERT INTO produto VALUES (6,'Detetive',49.00,0,'Nova Vers„o do Jogo',1001)
-INSERT INTO produto VALUES (7,'Chocolate com PaÁoquinha',6.00,0,'Barra',1002)
+INSERT INTO produto VALUES (6,'Detetive',49.00,0,'Nova Vers√£o do Jogo',1001)
+INSERT INTO produto VALUES (7,'Chocolate com Pa√ßoquinha',6.00,0,'Barra',1002)
 INSERT INTO produto VALUES (8,'Galak',5.00,65,'Barra',1002)
 INSERT INTO pedido VALUES (99001,33601,1,1,'07/03/2023')
 INSERT INTO pedido VALUES (99001,33601,2,1,'07/03/2023')
@@ -86,7 +86,7 @@ JOIN cliente ON pedido.cod_cli = cliente.codigo
 JOIN produto ON pedido.cod_prod = produto.codigo
 WHERE cliente.nome = 'Maria Clara';
 
--- 2. Consultar quais brinquedos n„o tem itens em estoque.
+-- 2. Consultar quais brinquedos n√£o tem itens em estoque.
 SELECT 
     produto.nome AS nome_produto,
     produto.qtd_estoque AS quantidade_estoque,
@@ -96,7 +96,7 @@ JOIN fornecedor ON produto.cod_forn = fornecedor.codigo
 WHERE produto.qtd_estoque = 0
 AND fornecedor.atividade LIKE '%Brinquedo%';
 
--- 3. Consultar quais nome e descriÁıes de produtos que n„o est„o em pedidos
+-- 3. Consultar quais nome e descri√ß√µes de produtos que n√£o est√£o em pedidos
 SELECT 
     produto.nome AS nome_produto,
     produto.descricao AS descricao_produto
@@ -104,7 +104,7 @@ FROM produto
 LEFT JOIN pedido ON produto.codigo = pedido.cod_prod
 WHERE pedido.codigo IS NULL;
 
--- 4. Alterar a quantidade em estoque do faqueiro para 10 peÁas.
+-- 4. Alterar a quantidade em estoque do faqueiro para 10 pe√ßas.
 UPDATE produto
 SET qtd_estoque = 10
 WHERE nome = 'Faqueiro';
@@ -121,7 +121,7 @@ SELECT
 FROM fornecedor
 WHERE fornecedor.atividade IN ('Brinquedo', 'Chocolate');
 
--- 7. Consultar nome e desconto de 25% no preÁo dos produtos que custam menos de R$50,00
+-- 7. Consultar nome e desconto de 25% no pre√ßo dos produtos que custam menos de R$50,00
 SELECT 
     produto.nome AS nome_produto,
     produto.valor_unitario AS valor_unitario,
@@ -129,7 +129,7 @@ SELECT
 FROM produto
 WHERE produto.valor_unitario < 50;
 
--- 8. Consultar nome e aumento de 10% no preÁo dos produtos que custam mais de R$100,00
+-- 8. Consultar nome e aumento de 10% no pre√ßo dos produtos que custam mais de R$100,00
 SELECT 
     produto.nome AS nome_produto,
     produto.valor_unitario AS valor_unitario,
@@ -148,7 +148,7 @@ FROM pedido
 JOIN produto ON pedido.cod_prod = produto.codigo
 WHERE pedido.codigo = 99001;
 
--- 10. Consultar CÛdigo do pedido, nome do cliente e idade atual do cliente
+-- 10. Consultar C√≥digo do pedido, nome do cliente e idade atual do cliente
 SELECT 
     pedido.codigo AS codigo_pedido,
     cliente.nome AS nome_cliente,
@@ -169,13 +169,13 @@ FROM produto
 JOIN fornecedor ON produto.cod_forn = fornecedor.codigo
 WHERE produto.valor_unitario = (SELECT MAX(valor_unitario) FROM produto);
 
--- 12. Consultar a mÈdia dos valores cujos produtos ainda est„o em estoque
+-- 12. Consultar a m√©dia dos valores cujos produtos ainda est√£o em estoque
 SELECT 
     AVG(produto.valor_unitario) AS media_valor_estoque
 FROM produto
 WHERE produto.qtd_estoque > 0;
 
---13. Consultar o nome do cliente, endereÁo composto por logradouro e n˙mero, o valor unit·rio do produto, o valor total (Quantidade * valor unitario) da compra do cliente de nome Maria Clara
+--13. Consultar o nome do cliente, endere√ßo composto por logradouro e n√∫mero, o valor unit√°rio do produto, o valor total (Quantidade * valor unitario) da compra do cliente de nome Maria Clara
 SELECT 
     cliente.nome AS nome_cliente,
     CONCAT(cliente.logradouro, ' ', cliente.numero) AS endereco_completo,
@@ -186,7 +186,7 @@ JOIN cliente ON pedido.cod_cli = cliente.codigo
 JOIN produto ON pedido.cod_prod = produto.codigo
 WHERE cliente.nome = 'Maria Clara';
 
--- 14. Considerando que o pedido de Maria Clara foi entregue 15/03/2023, consultar quantos dias houve de atraso. A cl·usula do WHERE deve ser o nome da cliente.
+-- 14. Considerando que o pedido de Maria Clara foi entregue 15/03/2023, consultar quantos dias houve de atraso. A cl√°usula do WHERE deve ser o nome da cliente.
 SELECT 
     cliente.nome AS nome_cliente,
     DATEDIFF(DAY, pedido.previsao_ent, '2023-03-15') AS dias_atraso
@@ -194,17 +194,10 @@ FROM pedido
 JOIN cliente ON pedido.cod_cli = cliente.codigo
 WHERE cliente.nome = 'Maria Clara';
 
--- 15. Consultar qual a nova data de entrega para o pedido de Alberto% sabendo que se pediu 9 dias a mais. A cl·usula do WHERE deve ser o nome do cliente. A data deve ser exibida no formato dd/mm/aaaa.
+-- 15. Consultar qual a nova data de entrega para o pedido de Alberto% sabendo que se pediu 9 dias a mais. A cl√°usula do WHERE deve ser o nome do cliente. A data deve ser exibida no formato dd/mm/aaaa.
 SELECT 
     cliente.nome AS nome_cliente,
     CONVERT(VARCHAR(10), DATEADD(DAY, 9, pedido.previsao_ent), 103) AS nova_data_entrega
 FROM pedido
 JOIN cliente ON pedido.cod_cli = cliente.codigo
 WHERE cliente.nome = 'Alberto Souza';
-
-
-
-
-
-
-
