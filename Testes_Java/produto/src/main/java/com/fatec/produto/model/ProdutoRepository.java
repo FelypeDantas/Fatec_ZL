122b0ad40a8a345a8b380580d4d813d13a57a5c2
Felype <felyped03@gmail.com>
2024-12-16 16:20:58 -0300

    Delete Testes_Java directory

--- Testes_Java/produto/src/main/java/com/fatec/produto/model/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.produto.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository <Produto, Long> {
-
-}
